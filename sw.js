871e62690c56d952f1d9eb08c72c54259ee28c65
aryaabdoli22-hue <aryaabdoli22@gmail.com>
2025-09-17 04:26:43 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,47 @@
+const CACHE_NAME = 'notes-cache-v1';
+const FILES_TO_CACHE = [
+  '/two-sided-notes/',
+  '/two-sided-notes/index.html',
+  '/two-sided-notes/manifest.json',
+  '/two-sided-notes/icons/icon-192.png',
+  '/two-sided-notes/icons/icon-512.png'
+];
+
+// Install Service Worker
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('notes-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html'
-            ]);
-        })
-    );
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('[ServiceWorker] Pre-caching offline page');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
         })
-    );
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch Handler
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
